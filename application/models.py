f6ecc80404f44a7ba86b6526d4277665cbb3816f
Tanisha Maheshwari <tanishamaheshwari@Tanishas-MacBook-Air.local>
2024-08-22 09:27:59 +0530

    mad2

---
+++ application/models.py
@@ -0,0 +1,49 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import UserMixin, RoleMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=False)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    balance = db.Column(db.Integer)
+    fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False)
+    roles = db.relationship('Role', secondary='roles_users',
+                         backref=db.backref('users', lazy='dynamic'))
+    sections = db.relationship('Section', secondary='users_sections',
+                              backref=db.backref('users', lazy='dynamic'))
+    books = db.relationship('Book', secondary='users_books',
+                           backref=db.backref('users', lazy='dynamic'))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class Section(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    books = db.relationship('Book', backref='section', lazy=True)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    section_id = db.Column(db.Integer, db.ForeignKey('section.id'), nullable=False)
+
+class UsersSections(db.Model):
+    __tablename__ = 'users_sections'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column('user_id', db.Integer(), db.ForeignKey('user.id'))
+    section_id = db.Column('section_id', db.Integer(), db.ForeignKey('section.id'))
+
+class UsersBooks(db.Model):
+    __tablename__ = 'users_books'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column('user_id', db.Integer(), db.ForeignKey('user.id'))
+    book_id = db.Column('book_id', db.Integer(), db.ForeignKey('book.id'))
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from application.models import db
+from config import DevelopmentConfig
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    db.init_app(app)
     with app.app_context():
         import application.views
 
