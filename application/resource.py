b76f2761f696b7ed1c9e67e69649f4d0f4c9eb27
Tanisha Maheshwari <tanishamaheshwari@Tanishas-MacBook-Air.local>
2024-08-23 09:57:56 +0530

    mad2

--- application/resource.py
+++ application/resource.py
@@ -1,13 +1,20 @@
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from application.models import Section, db
 
 api = Api(prefix='/api')
 
-class StudyMaterial(Resource):
+parser = reqparse.RequestParser()
+parser.add_argument("name", type=str, help="Name must be a string.")
+parser.add_argument("description", type=str, help="Name must be a string.")
+
+class Section(Resource):
     def get(self):
-        return{"message": "hello from api"}
+        return{"message": "hello from Section"}
+    
+    def post(self):
+        args= parser.parse_args()
+        section_add = Section(**args)
+        db.session.add(section_add)
+        db.session.commit()
         
-
-
-
-
-api.add_resource(StudyMaterial, '/study_material')
+api.add_resource(Section, '/section')
